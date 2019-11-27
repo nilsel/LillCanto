897b073a61a165f39049729f1a3c45a8490e0705
Nils P. Ellingsen <q2i@storebrand.no>
2019-11-27 20:34:55 +0100

    move menu to markdown

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function loadMd(kind, targetElement) {
+function loadMd(kind, targetElementId) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
       console.log('success!', xhr.respone);
-      document.getElementById(targetElement).innerHTML = marked(xhr.response);
-      document.getElementById(targetElement).classList = 'col loaded';
+      document.getElementById(targetElementId).innerHTML = marked(xhr.response);
+      document.getElementById(targetElementId).classList = 'col loaded';
     } else {
       console.log('The request failed!');
     }
---
+++ markdown/meny.md
@@ -0,0 +1,5 @@
+* [Hjem](./index.html)
+* [Aktivitetskalender](./index.html)
+* [Bli medlem](./bli-medlem.html)
+* [Kontakt oss](./kontakt.html)
+
