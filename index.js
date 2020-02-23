73aef6f2171f0ca8db91576def913dbb893e5880
Nils P. Ellingsen <q2i@storebrand.no>
2020-02-23 21:22:01 +0100

    Fixes for IE11

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function loadMd(kind, targetElementId) {
     if (xhr.status >= 200 && xhr.status < 300) {
       console.log('success!', xhr.respone);
       document.getElementById(targetElementId).innerHTML = marked(xhr.response);
-      document.getElementById(targetElementId).classList = 'col loaded';
+      document.getElementById(targetElementId).className += ' loaded';
     } else {
       console.log('The request failed!');
     }
