80c911c2994165f06b5360b66e422c174b027cc8
Nils P. Ellingsen <q2i@storebrand.no>
2021-11-03 21:06:58 +0100

    Update Marked usage (breaking change in 4.0.0)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function loadMd(kind, targetElementId) {
   xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
       console.log('success!', xhr.respone);
-      document.getElementById(targetElementId).innerHTML = marked(xhr.response);
+      document.getElementById(targetElementId).innerHTML = marked.parse(xhr.response);
       document.getElementById(targetElementId).className += ' loaded';
     } else {
       console.log('The request failed!');
